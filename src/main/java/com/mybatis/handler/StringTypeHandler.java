ecc31b3e5e7b1c23bdd728f382f9e5f37f66409d
xionggp <xionggp@tigerbrokers.com>
2017-04-06 14:17:56 +0800

    add： type handler

---
+++ src/main/java/com/mybatis/handler/StringTypeHandler.java
@@ -0,0 +1,34 @@
+package com.mybatis.handler;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+
+import java.sql.*;
+
+/**
+ * Created by xionggp on 17/4/6.
+ */
+@MappedJdbcTypes(JdbcType.VARCHAR)
+public class StringTypeHandler extends BaseTypeHandler<String> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, parameter);
+    }
+
+    @Override
+    public String getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return rs.getString(columnName);
+    }
+
+    @Override
+    public String getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return rs.getString(columnIndex);
+    }
+
+    @Override
+    public String getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return cs.getString(columnIndex);
+    }
+}
